124df7f0159b39279fef03b152edf8f6e6acab66
Brooks Wimer <wimerbrooks@gmail.com>
2024-02-15 23:39:52 -0500

    could not get full points on part 4, returning to part two with simpilar approach

--- assigns/assign03/assign03_02.ml
+++ assigns/assign03/assign03_02.ml
@@ -28,8 +28,8 @@
    2. It has the same number of `Cons`s, `Fork`s and `Nil`s as `f`.
 
    Example:
-   let f = Cons (2, Fork(4, Cons(3, Nil), Cons (5, Nil)))
-   let g = Fork (4, Cons (2, Cons (3, Nil)), Cons(5, Nil))
+   let f = Cons (2, Fork(4, Cons(3, Nil), Cons (5, Nil))) in 
+   let g = Fork (4, Cons (2, Cons (3, Nil)), Cons(5, Nil)) in 
    let _ = assert (delay_cons f = g)
 
    NOTE: the output does not need to look exactly like this. It just
@@ -42,5 +42,27 @@ type 'a forklist
   | Cons of 'a * 'a forklist
   | Fork of 'a * 'a forklist * 'a forklist
 
-let delay_cons (f : int forklist) : int forklist =
-  assert false (* TODO *)
+(*
+
+let rec insert_in_tailed x = function
+  | Nil -> Cons(x, Nil)  (* Inserting into Nil simply creates a Cons *)
+  | Cons(y, ys) as cons -> if x < y then Cons(x, cons) else Cons(y, insert_in_tailed x ys)
+  | Fork(y, lxs, rxs) -> if x < y then Fork(y, insert_in_tailed x lxs, rxs) else Fork(y, lxs, insert_in_tailed x rxs)
+
+(* delay_cons implementation *)
+let rec delay_cons (f : int forklist) : int forklist = match f with
+  | Nil -> Nil
+  | Cons(x, xs) -> insert_in_tailed x (delay_cons xs)  (* Insert x into the delayed version of xs *)
+  | Fork(x, lxs, rxs) -> 
+      let delayed_lxs = delay_cons lxs in
+      let delayed_rxs = delay_cons rxs in
+      insert_in_tailed x (Fork(x, delayed_lxs, delayed_rxs)) 
+
+*)
+
+let rec delay_cons (f : int forklist) : int forklist =
+  match fl with
+  | Nil | Cons (_, Nil) -> fl
+  | Cons (x, Fork (y, l, r)) -> Fork (y, delay_cons (Cons (x, l)), delay_cons r)
+  | Cons (x, y) -> Cons (x, delay_cons y)
+  | Fork (x, l, r) -> Fork (x, delay_cons l, delay_cons r)
